227ce9fd450252f53c708a45af3c0026937bc5d1
Sam Kutsyn <cod3ddot@proton.me>
2024-03-24 15:57:10 -0700

    basic api auth

--- src/lib/pocketbase/config.ts
+++ src/lib/pocketbase/config.ts
@@ -4,6 +4,12 @@ import { pb_url } from './consts';
 import { Project, Thought, TypedPocketBase } from './types';
 
 export const pb = new PocketBase(pb_url) as TypedPocketBase;
+
+const authData = await pb
+	.collection('users')
+	.authWithPassword('cod3d.dev', 'Vqh66Zyp5rR2zT1');
+
+pb.authStore.save(authData.token);
 pb.autoCancellation(false);
 
 export function getFileUrl(
