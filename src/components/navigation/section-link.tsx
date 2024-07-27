e226d4a0bd06beef6b2f9d97c1bbf4296146f9af
Cod3dDOT <cod3ddot@proton.me>
2024-07-26 17:31:58 -0700

    Update section-link.tsx

--- src/components/navigation/section-link.tsx
+++ src/components/navigation/section-link.tsx
@@ -10,9 +10,9 @@ export const SectionLink: React.FC<{ link: string; text: string }> = ({
 			hrefLang="en"
 			href={link}
 			className="relative flex group w-full justify-between items-center mb-8 overflow-hidden leading-none
-            hover:-skew-x-12 transition-transform"
+            hover:-skew-x-6 transition-transform"
 		>
-			<h2 className="md:text-[5.65rem] sm:text-[4rem] text-[2.83rem] group-hover:[text-shadow:4_4_0_#407cff] transition-all">
+			<h2 className="md:text-[5.65rem] sm:text-[4rem] text-[2.83rem] group-hover:[text-shadow:4px_4px_0px_#407cff] transition-all">
 				{text}
 			</h2>
 			<div className="relative w-8 h-8 group-hover:opacity-100 lg:opacity-0 transition-opacity">
