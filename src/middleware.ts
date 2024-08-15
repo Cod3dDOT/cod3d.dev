4adcd1cee1f3ca9736acfad4b7f6de84c765df97
cod3d <cod3ddot@proton.me>
2024-08-14 22:34:59 -0700

    better cors?

---
+++ src/middleware.ts
@@ -0,0 +1,50 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+const allowedOrigins = ['https://cod3d.dev'];
+
+const corsOptions = {
+	'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+	'Access-Control-Allow-Headers': 'Content-Type, Authorization'
+};
+
+export const config = {
+	matcher: [
+		/*
+		 * Match all request paths except for the ones starting with:
+		 * - _next/static (static files)
+		 * - _next/image (image optimization files)
+		 * - favicon.ico (favicon file)
+		 */
+		'/((?!_next/static|_next/image|favicon.ico).*)'
+	]
+};
+
+export function middleware(request: NextRequest) {
+	// Check the origin from the request
+	const origin = request.headers.get('origin') ?? '';
+	const isAllowedOrigin = allowedOrigins.includes(origin);
+
+	// Handle preflighted requests
+	const isPreflight = request.method === 'OPTIONS';
+
+	if (isPreflight) {
+		const preflightHeaders = {
+			...(isAllowedOrigin && { 'Access-Control-Allow-Origin': origin }),
+			...corsOptions
+		};
+		return NextResponse.json({}, { headers: preflightHeaders });
+	}
+
+	// Handle simple requests
+	const response = NextResponse.next();
+
+	if (isAllowedOrigin) {
+		response.headers.set('Access-Control-Allow-Origin', origin);
+	}
+
+	Object.entries(corsOptions).forEach(([key, value]) => {
+		response.headers.set(key, value);
+	});
+
+	return response;
+}
