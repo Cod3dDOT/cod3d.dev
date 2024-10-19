caf453e4ff8e46d2c19ac6dcb9c7740cacfc90b9
cod3d <64918545+Cod3dDOT@users.noreply.github.com>
2024-10-18 19:19:58 -0700

    Update useIsVisible.ts

--- src/lib/hooks/useIsVisible.ts
+++ src/lib/hooks/useIsVisible.ts
@@ -3,19 +3,20 @@ import { RefObject, useEffect, useMemo, useState } from 'react';
 export default function useIsVisible(ref: RefObject<HTMLElement>) {
 	const [isIntersecting, setIntersecting] = useState(false);
 
-	const observer = useMemo(
-		() =>
-			new IntersectionObserver(([entry]) =>
-				setIntersecting(entry.isIntersecting)
-			),
-		[ref]
-	);
+	const observer = useMemo(() => {
+		if (!ref.current) return;
+
+		return new IntersectionObserver(([entry]) =>
+			setIntersecting(entry.isIntersecting)
+		);
+	}, [ref.current]);
 
 	useEffect(() => {
 		if (!ref.current) return;
-		observer.observe(ref.current);
-		return () => observer.disconnect();
-	}, []);
+
+		observer?.observe(ref.current);
+		return () => observer?.disconnect();
+	}, [ref.current]);
 
 	return isIntersecting;
 }
