9858b18e02dc839701133547748a89e68bc01697
cod3d <64918545+Cod3dDOT@users.noreply.github.com>
2024-10-20 22:46:19 -0700

    Update syncedRotationWrapper.tsx
    fix hydration errors

--- src/components/pages/home/syncedRotationWrapper.tsx
+++ src/components/pages/home/syncedRotationWrapper.tsx
@@ -1,13 +1,27 @@
 'use client';
 
+import { clsx } from 'clsx';
+import { useEffect, useState } from 'react';
+
+// needs to be a client component for the date to work
 export const SyncedRotationWrapper: React.FC<{ children: React.ReactNode }> = ({
 	children
 }) => {
-	// needs to be a client component for the date to work
-	const seconds = new Date().getSeconds();
+	const [opacity, setOpacity] = useState('opacity-0');
+	const [seconds, setSeconds] = useState(0);
+
+	useEffect(() => {
+		setSeconds(new Date().getSeconds());
+		setOpacity('opacity-100');
+	}, []);
 
 	return (
-		<div className="fixed inset-0 -z-10 overflow-hidden flex lg:justify-center lg:items-center animate-out fade-out-100 fade-in-0">
+		<div
+			className={clsx(
+				'fixed inset-0 -z-10 overflow-hidden flex lg:justify-center lg:items-center transition-opacity duration-1000',
+				opacity
+			)}
+		>
 			<style>
 				{`svg {
                     --delay: calc(${seconds} * -1s);
