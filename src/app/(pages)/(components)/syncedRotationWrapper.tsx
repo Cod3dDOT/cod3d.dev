35316e2e432054f431b9a15f80478ffef50d9ac8
cod3d <64918545+Cod3dDOT@users.noreply.github.com>
2025-04-15 16:20:56 -0700

    fix(home page): synched blob offset now respects prefers-reduced-motion

--- src/app/(pages)/(components)/syncedRotationWrapper.tsx
+++ src/app/(pages)/(components)/syncedRotationWrapper.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useDeviceDetection } from "@/lib/hooks/useDeviceDetection";
 import { useEffect, useState } from "react";
 
 // needs to be a client component for the date to work
@@ -8,9 +9,12 @@ export const SyncedRotationWrapper: React.FC<{ children: React.ReactNode }> = ({
 }) => {
 	const [seconds, setSeconds] = useState(0);
 
+	const { isReducedMotion } = useDeviceDetection();
+	const animationDuration = isReducedMotion ? 512 : 32;
+
 	useEffect(() => {
-		setSeconds((new Date().getSeconds() % 32) * -1);
-	}, []);
+		setSeconds((new Date().getSeconds() % animationDuration) * -1);
+	}, [animationDuration]);
 
 	return (
 		<div className="-inset-full -z-10 fixed flex animate-in-reduced items-center justify-center overflow-hidden lg:inset-0">
--- src/app/(pages)/thoughts/[slug]/(components)/markdown/wrapper.tsx
+++ src/app/(pages)/thoughts/[slug]/(components)/markdown/wrapper.tsx
@@ -14,83 +14,76 @@ import { MarkdownCodeBlock } from "./code";
 import { MarkdownImage } from "./image";
 
 type MarkdownWrapperProps = {
-    markdown: string;
-    images: string[];
+	markdown: string;
+	images: string[];
 };
 
 export const MarkdownWrapper: React.FC<MarkdownWrapperProps> = ({
-    images,
-    markdown,
+	images,
+	markdown
 }) => {
-    return (
-        <section className="relative opacity-0 [--delay:500ms] motion-safe:animate-in md:px-10 xl:flex">
-            <div
-                className={cn(
-                    "prose lg:prose-xl prose-neutral prose-amber dark:prose-invert max-w-none",
-                    "prose-headings:font-light prose-h4:text-[larger]",
-                    "prose-a:transition-colors prose-a:hover:text-accent-blue",
-                    "prose-pre:bg-transparent prose-pre:text-foreground",
-                    "prose-inline-code:before:content-none prose-inline-code:after:content-none", // removes `` from inline code blocks
-                    "prose-inline-code:text-[smaller] not-print:prose-inline-code:mx-1 prose-inline-code:rounded-md not-print:prose-inline-code:bg-container not-print:prose-inline-code:p-1",
-                    "md:prose-figure:mx-10",
-                    "md:first-letter:text-6xl xl:max-w-prose [&>*:not(figure)]:px-10"
-                )}
-            >
-                <Markdown
-                    components={{
-                        h1() {
-                            return <></>;
-                        },
-                        pre(props: JSX.IntrinsicElements["pre"] & ExtraProps) {
-                            return <MarkdownCodeBlock {...props} />;
-                        },
-                        img(props: JSX.IntrinsicElements["img"] & ExtraProps) {
-                            const src = props.src;
+	return (
+		<section className="relative opacity-0 [--delay:500ms] motion-safe:animate-in md:px-10 xl:flex">
+			<div
+				className={cn(
+					"prose lg:prose-xl prose-neutral prose-amber dark:prose-invert max-w-none",
+					"prose-headings:font-light prose-h4:text-[larger]",
+					"prose-a:transition-colors prose-a:hover:text-accent-blue",
+					"prose-pre:bg-transparent prose-pre:text-foreground",
+					"prose-inline-code:before:content-none prose-inline-code:after:content-none", // removes `` from inline code blocks
+					"not-print:prose-inline-code:mx-1 prose-inline-code:rounded-md not-print:prose-inline-code:bg-container not-print:prose-inline-code:p-1 prose-inline-code:text-[smaller]",
+					"md:prose-figure:mx-10",
+					"md:first-letter:text-6xl xl:max-w-prose [&>*:not(figure)]:px-10"
+				)}
+			>
+				<Markdown
+					components={{
+						h1() {
+							return <></>;
+						},
+						pre(props: JSX.IntrinsicElements["pre"] & ExtraProps) {
+							return <MarkdownCodeBlock {...props} />;
+						},
+						img(props: JSX.IntrinsicElements["img"] & ExtraProps) {
+							const src = props.src;
 
-                            return (
-                                <MarkdownImage
-                                    src={src}
-                                    allImages={images}
-                                    alt={props.alt}
-                                />
-                            );
-                        },
-                        p(props: JSX.IntrinsicElements["p"] & ExtraProps) {
-                            if (!props.children) return <></>;
+							return (
+								<MarkdownImage src={src} allImages={images} alt={props.alt} />
+							);
+						},
+						p(props: JSX.IntrinsicElements["p"] & ExtraProps) {
+							if (!props.children) return <></>;
 
-                            const child = props.node?.children[0];
-                            if (
-                                child?.type === "element" &&
-                                child?.tagName === "img"
-                            ) {
-                                return <>{props.children}</>;
-                            }
-                            return <p>{props.children}</p>;
-                        },
-                    }}
-                    remarkPlugins={[
-                        remarkFrontmatter,
-                        remarkMath,
-                        remarkCallout,
-                        [remarkHeadingId, { defaults: true }],
-                    ]}
-                    rehypePlugins={[
-                        [rehypeKatex, { output: "mathml" }],
-                        rehypeHighlight,
-                        [
-                            rehypeHighlightCodeLines,
-                            {
-                                showLineNumbers: true,
-                            },
-                        ],
-                    ]}
-                >
-                    {markdown}
-                </Markdown>
-            </div>
-            <div className="-translate-y-1/2 sticky top-1/2 left-1/2 mt-60 hidden translate-x-8 self-start overflow-hidden xl:block 2xl:translate-x-1/2">
-                <TableOfContents />
-            </div>
-        </section>
-    );
+							const child = props.node?.children[0];
+							if (child?.type === "element" && child?.tagName === "img") {
+								return <>{props.children}</>;
+							}
+							return <p>{props.children}</p>;
+						}
+					}}
+					remarkPlugins={[
+						remarkFrontmatter,
+						remarkMath,
+						remarkCallout,
+						[remarkHeadingId, { defaults: true }]
+					]}
+					rehypePlugins={[
+						[rehypeKatex, { output: "mathml" }],
+						rehypeHighlight,
+						[
+							rehypeHighlightCodeLines,
+							{
+								showLineNumbers: true
+							}
+						]
+					]}
+				>
+					{markdown}
+				</Markdown>
+			</div>
+			<div className="-translate-y-1/2 sticky top-1/2 left-1/2 mt-60 hidden translate-x-8 self-start overflow-hidden xl:block 2xl:translate-x-1/2">
+				<TableOfContents />
+			</div>
+		</section>
+	);
 };
