29ee3e7f499c844910b7dca2d66639f2773b4102
cod3d <64918545+Cod3dDOT@users.noreply.github.com>
2025-05-19 15:56:59 -0700

    feat(nibblet): nibbling at the nibblet

--- public/nibblet/scripts/nibblet-test.js
+++ public/nibblet/scripts/nibblet-test.js
@@ -1,4 +1,15 @@
-() => {
+const main = () => {
 	document.title = "HELLO FROM NIBBLET";
-	return true;
+	return {
+		success: true,
+		message: "hello from nibblet"
+	};
 };
+
+document.addEventListener(
+	"script-utils-event[RUN]",
+	document.dispatchEvent(
+		new CustomEvent("script-utils-event[RUN]", { detail: main() })
+	),
+	false
+);
