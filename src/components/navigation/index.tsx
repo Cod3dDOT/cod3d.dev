ee4a77e4e676e8250ada45f2a03fcd20c2acf57d
cod3d <64918545+Cod3dDOT@users.noreply.github.com>
2025-05-26 23:37:37 -0700

    fix(lint): biome import sort

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -4,8 +4,8 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import NavContactsShowcase from "./contact";
 import { NavigationContainer } from "./common/container";
+import NavContactsShowcase from "./contact";
 import NavProjectsShowcase from "./projects";
 import NavThoughtsShowcase from "./thoughts";
 
