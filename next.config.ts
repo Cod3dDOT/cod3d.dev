b13b245b12bda1534baf184ba06962cc3a282878
cod3d <64918545+Cod3dDOT@users.noreply.github.com>
2025-08-08 00:00:19 -0700

    fix(hsts): use cloudflare's hsts

--- next.config.ts
+++ next.config.ts
@@ -70,10 +70,6 @@ const nextConfig: NextConfig = {
 						value: "nosniff"
 					},
 					{
-						key: "Strict-Transport-Security",
-						value: "max-age=63072000; includeSubDomains; preload"
-					},
-					{
 						key: "Permissions-Policy",
 						value:
 							"fullscreen=(self),picture-in-picture=(self),clipboard-write=(self),attribution-reporting=(self),compute-pressure=(self),accelerometer=(),autoplay=(),bluetooth=(),browsing-topics=(),camera=(),display-capture=(),gamepad=(),geolocation=(),gyroscope=(),hid=(),magnetometer=(),microphone=(),midi=(),otp-credentials=(),payment=(),serial=(),usb=(),xr-spatial-tracking=()"
